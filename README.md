e78df375619bdb063ad661c3263c641bbc5029d1
nielsengelen <niels@foonet.be>
2016-05-27 13:13:32 +0200

    v9 design update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Veeam Availability Suite clickable demo's
+# Veeam Availability Suite clickable demos
 
-These demo's are created to help in showing the features available in the Veeam Availability Suite. They can be used at events, partners, customers,... and will show how easy to use the Veeam Availability Suite is.
+These demos are created to help in showing the features available in the Veeam Availability Suite. They can be used at events, partners, customers,... and will show how easy to use the Veeam Availability Suite is.
 
 From the index select a specific feature and just click on the boxes to proceed to the next (or previous) step. Once finished it will return to the global index. By clicking on the upper logo the whole index page will refresh.
 
-All demo's are fully HTML5 and therefor don't require Flash or Silverlight to work.
+All demos are fully HTML5 and therefor don't require Flash or Silverlight to work.
--- js/modifier.js
+++ js/modifier.js
@@ -55,9 +55,9 @@ function loadInfo(value) {
 			$('#rightcolumn2').width('100%');
 			$('#rightcolumn2').html('<iframe src="lab/boss_emc/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
-		case 'bfsshp':
+		case 'bfsshpe':
 			$('#rightcolumn2').width('100%');
-			$('#rightcolumn2').html('<iframe src="lab/boss_hp/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
+			$('#rightcolumn2').html('<iframe src="lab/boss_hpe/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
 		case 'bfssna':
 			$('#rightcolumn2').width('100%');
@@ -71,9 +71,9 @@ function loadInfo(value) {
 			$('#rightcolumn2').width('100%');
 			$('#rightcolumn2').html('<iframe src="lab/exagrid/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
-		case 'hpstoreonce':
+		case 'storeonce':
 			$('#rightcolumn2').width('100%');
-			$('#rightcolumn2').html('<iframe src="lab/hpstoreonce/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
+			$('#rightcolumn2').html('<iframe src="lab/storeonce/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
 		case 'fp':
 			$('#rightcolumn2').width('100%');
@@ -123,9 +123,9 @@ function loadInfo(value) {
 			$('#rightcolumn4').width('100%');
 			$('#rightcolumn4').html('<iframe src="lab/virtuallab/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
-		case 'vlbss':
+		case 'appss':
 			$('#rightcolumn4').width('100%');
-			$('#rightcolumn4').html('<iframe src="lab/virtuallab_ss/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
+			$('#rightcolumn4').html('<iframe src="lab/applicationgroup-ss/tutorial.html?autofit=true" width="100%" height="100%"></iframe>');
 			break;
 		case 'appb':
 			$('#rightcolumn4').width('100%');
@@ -160,11 +160,11 @@ function reset() {
 	$('#rightcolumn1').width('85%');
 	$('#leftcolumn1').html('<img src="images/hsr.png" class="menuimg" /><span class="menutitle"><br />High Speed Recovery</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo:loadInfo(\'ivm\')">Instant VM Recovery</a></li><li><a href="javascript:loadInfo:loadInfo(\'iflrl\')">Instant File-level Recovery<br />(Linux)</a></li><li><a href="javascript:loadInfo:loadInfo(\'iflrw\')">Instant File-level Recovery<br />(Windows)</a></li><br /><span class="menuheader">Veeam Explorers:</span><br /><li><a href="javascript:loadInfo:loadInfo(\'vead\')">Microsoft Active Directory <span class="v8feature">(v8)</span></a></li><li><a href="javascript:loadInfo:loadInfo(\'vex\')">Microsoft Exchange</a></li><li><a href="javascript:loadInfo:loadInfo(\'vesp\')">Microsoft SharePoint</a></li><li><a href="javascript:loadInfo:loadInfo(\'vesql\')">Microsoft SQL Server <span class="v8feature">(v8)</span></a></li><br /><li><a href="javascript:loadInfo(\'veora\')">Oracle <span class="v8feature">(v9)</span></a></li></ul>');
 	$('#rightcolumn2').width('85%');
-	$('#leftcolumn2').html('<img src="images/dla.png" class="menuimg" /><span class="menutitle"><br />Data Loss Avoidance</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo(\'bj\')">Backup Job</a></li><li><a href="javascript:loadInfo(\'bcj\')">Backup Copy Job<br />(WAN acceleration)</a></li><li><a href="javascript:loadInfo(\'rj\')">Replication Job</a></li><br /><li><a href="javascript:loadInfo(\'bioc\')">Backup I/O Control <span class="v8feature">(v8)</a></span></li><li><a href="javascript:loadInfo(\'etee\')">End-to-End Encryption <span class="v8feature">(v8)</a></span></li><li><a href="javascript:loadInfo(\'fp\')">Failover Plan <span class="v8feature">(v8)</a></span></li><li><a href="javascript:loadInfo(\'vcc\')">Veeam Cloud Connect <span class="v8feature">(v8)</span></a></li><br /><li><a href="javascript:loadInfo(\'sobr\')">Scale-Out Backup Repository <span class="v8feature">(v9)</a></span></li><li><a href="javascript:loadInfo(\'pvmbc\')">Per-VM Backup Chain <span class="v8feature">(v9)</a></span></li><br /><span class="menuheader">Backup from Storage Snapshots:</span><br /><li><a href="javascript:loadInfo(\'bfssemc\')">EMC VNX(e)</a> <span class="v8feature">(v9)</span></li><li><a href="javascript:loadInfo(\'bfsshp\')">HP StoreVirtual/StoreServ</a></li><li><a href="javascript:loadInfo(\'bfssna\')">NetApp FAS</a> <span class="v8feature">(v8)</span></li><br /><span class="menuheader">Deduplicating storage:</span><br /><li><a href="javascript:loadInfo(\'emcdd\')">EMC Data Domain Boost <span class="v8feature">(v8)</span></a></li><li><a href="javascript:loadInfo(\'exagrid\')">Exagrid <span class="v8feature">(v8)</span></a></li><li><a href="javascript:loadInfo(\'hpstoreonce\')">HP StoreOnce Catalyst <span class="v8feature">(v9)</span></a></li><br /><span class="menuheader">Native Tape Support:</span><br /><li><a href="javascript:loadInfo(\'ntt\')">Backup to Tape</a></li><li><a href="javascript:loadInfo(\'tapegfs\')">GFS media pool <span class="v8feature">(v9)</span></a></li><li><a href="javascript:loadInfo(\'tapeglobal\')">Global media pool <span class="v8feature">(v9)</span></a></li></ul>');
+	$('#leftcolumn2').html('<img src="images/dla.png" class="menuimg" /><span class="menutitle"><br />Data Loss Avoidance</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo(\'bj\')">Backup Job</a></li><li><a href="javascript:loadInfo(\'bcj\')">Backup Copy Job<br />(WAN acceleration)</a></li><li><a href="javascript:loadInfo(\'rj\')">Replication Job</a></li><br /><li><a href="javascript:loadInfo(\'bioc\')">Backup I/O Control <span class="v8feature">(v8)</a></span></li><li><a href="javascript:loadInfo(\'fp\')">Failover Plan <span class="v8feature">(v8)</a></span></li><li><a href="javascript:loadInfo(\'vcc\')">Veeam Cloud Connect <span class="v8feature">(v9)</span></a></li><br /><li><a href="javascript:loadInfo(\'sobr\')">Scale-Out Backup Repository <span class="v8feature">(v9)</a></span></li><li><a href="javascript:loadInfo(\'pvmbc\')">Per-VM Backup Chain <span class="v8feature">(v9)</a></span></li><br /><span class="menuheader">Backup from Storage Snapshots:</span><br /><li><a href="javascript:loadInfo(\'bfssemc\')">EMC VNX(e)</a> <span class="v8feature">(v9)</span></li><li><a href="javascript:loadInfo(\'bfsshpe\')">HPE StoreVirtual/StoreServ</a></li><li><a href="javascript:loadInfo(\'bfssna\')">NetApp FAS</a> <span class="v8feature">(v8)</span></li><br /><span class="menuheader">Deduplicating storage:</span><br /><li><a href="javascript:loadInfo(\'emcdd\')">EMC Data Domain Boost <span class="v8feature">(v8)</span></a></li><li><a href="javascript:loadInfo(\'exagrid\')">Exagrid <span class="v8feature">(v8)</span></a></li><li><a href="javascript:loadInfo(\'storeonce\')">HPE StoreOnce Catalyst <span class="v8feature">(v9)</span></a></li><br /><span class="menuheader">Native Tape Support:</span><br /><li><a href="javascript:loadInfo(\'ntt\')">Backup to Tape</a></li><li><a href="javascript:loadInfo(\'tapegfs\')">GFS media pool <span class="v8feature">(v9)</span></a></li><li><a href="javascript:loadInfo(\'tapeglobal\')">Global media pool <span class="v8feature">(v9)</span></a></li></ul>');
 	$('#rightcolumn3').width('85%');
-	$('#leftcolumn3').html('<img src="images/vp.png" class="menuimg" /><span class="menutitle"><br />Verified Protection</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo:loadInfo(\'srb\')">SureBackup</a></li><li><a href="javascript:loadInfo:loadInfo(\'srr\')">SureReplica</a></li></ul>');
+	$('#leftcolumn3').html('<img src="images/vp.png" class="menuimg" /><span class="menutitle"><br />Verified Recoverability</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo:loadInfo(\'srb\')">SureBackup</a></li><li><a href="javascript:loadInfo:loadInfo(\'srr\')">SureReplica</a></li></ul>');
 	$('#rightcolumn4').width('85%');
-	$('#leftcolumn4').html('<img src="images/ld.png" class="menuimg" /><span class="menutitle"><br />Leveraged Data</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo(\'appb\')">Application Groups (Backups)</a></li><li><a href="javascript:loadInfo(\'appr\')">Application Groups (Replicas)</a></li><br /><span class="menuheader">On-Demand Sandbox:</span><br /><li><a href="javascript:loadInfo(\'vlb\')">From Backups</a></li><li><a href="javascript:loadInfo(\'vlbss\')">From Storage Snapshots <span class="v8feature">(v9)</span></a></li></ul>');
+	$('#leftcolumn4').html('<img src="images/ld.png" class="menuimg" /><span class="menutitle"><br />Leveraged Data</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo(\'vlb\')">Virtual Lab</a></li><br /><span class="menuheader">On-Demand Sandbox:</span><br /><li><a href="javascript:loadInfo(\'appb\')">Application Groups (Backups)</a></li><li><a href="javascript:loadInfo(\'appr\')">Application Groups (Replicas)</a></li><li><a href="javascript:loadInfo(\'appss\')">Application Groups (Storage Snapshot) <span class="v8feature">(v9)</span></a></a></li></ul>');
 	$('#rightcolumn5').width('85%');
 	$('#leftcolumn5').html('<img src="images/cv.png" class="menuimg" /><span class="menutitle"><br />Complete Visibility</span><br /><ul class="menuitems"><li><a href="javascript:loadInfo:loadInfo(\'vone\')">Veeam One</a></li><li><a href="javascript:loadInfo:loadInfo(\'vmp\')">Veeam Mangement Pack</a></li></ul>');
 }
